73ae8ab168cde8f47d0713d2ba9aaeefa077350b
Aswin Kumar .R <r.aswinkumar1994@gmail.com>
2016-06-30 10:44:17 +0530

    uploaded index php

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>Login</title>
+  
+
+  
+  
+
+  
+</head>
+
+<body>
+
+
+    <?php
+
+
+
+    echo "Hello PHP worlds ! "; 
+
+
+    ?>
+
+
+  
+
+
+</body>
+</html>
